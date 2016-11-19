f57a2455d800dea102530ca0707550c70648b4ba
Laurent Almeras <lalmeras@gmail.com>
2016-11-19 22:44:48 +0100

    Bump version: 0.1.0 → 0.2.0-dev

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='squiddee',
-    version='0.1.0',
+    version='0.2.0-dev',
     description="Command-line squid wrapper",
     long_description=readme + '\n\n' + history,
     author="Laurent Almeras",
--- squiddee/__init__.py
+++ squiddee/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Laurent Almeras'
 __email__ = 'lalmeras@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0-dev'
